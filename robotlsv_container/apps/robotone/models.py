577157f672ee6b2edb0f779385246887fac7bf3c
rafael barboza <barboza.rafael.p@gmail.com>
2018-07-09 09:59:50 -0500

    add model, registered in setting and admin

--- robotlsv_container/apps/robotone/models.py
+++ robotlsv_container/apps/robotone/models.py
@@ -1,3 +1,24 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+
+class Robot(models.Model):
+    started=models.DateTimeField()
+    finished=models.DateTimeField()
+    response=models.CharField(max_length=250)
+    type=(
+        (1,'robotA'),
+        (2,'robotB')
+    )
+    type=models.CharField(choices=type,default=1,max_length=50)
+    state = (
+        (1, 'Waiting'),
+        (2, 'Working'),
+        (3, 'Finished'),
+    )
+    status = models.CharField(choices=state,default=1,max_length=50)
+    user=models.ForeignKey(User,on_delete=models.CASCADE)
+
+    def __init__(self):
+        self.status='Waiting'
