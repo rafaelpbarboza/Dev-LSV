5d42db6539102e3d09dbe4fba1f76f3b59a7ae21
jesuslemus <jesuslemus0@gmail.com>
2018-07-10 15:44:23 -0500

    stage one of task added, but initializing robots thorugh view

--- robotlsv_container/apps/robotone/models.py
+++ robotlsv_container/apps/robotone/models.py
@@ -3,22 +3,20 @@ from django.db import models
 
 
 # Create your models here.
-class Robot(models.Model):
-    started = models.DateTimeField()
-    finished = models.DateTimeField()
-    response = models.CharField(max_length=250)
+class Robotmintor(models.Model):
+    started = models.DateTimeField(blank=True, null=True)
+    finished = models.DateTimeField(blank=True, null=True)
+    response = models.CharField(max_length=250, blank=True)
     TYPE = (
-        (1, 'robotA'),
-        (2, 'robotB')
+        ('1', 'robotA'),
+        ('2', 'robotB')
     )
     type = models.CharField(choices=TYPE, default=1, max_length=50)
-    state = (
-        (1, 'Waiting'),
-        (2, 'Working'),
-        (3, 'Finished'),
+    STATUS = (
+        ('1', 'Waiting'),
+        ('2', 'Working'),
+        ('3', 'Finished'),
     )
-    status = models.CharField(choices=state, default=1, max_length=50)
+    status = models.CharField(choices=STATUS, default=1, max_length=50)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
 
-    # def __init__(self):
-    #     self.status = 'Waiting'
